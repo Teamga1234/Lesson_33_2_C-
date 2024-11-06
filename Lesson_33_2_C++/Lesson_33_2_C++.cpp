e19c6d761d8f2575dfbd191eaf8d0ae1a2c621e3
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-11-06 18:07:04 +0200

    test 1

--- Lesson_33_2_C++/Lesson_33_2_C++.cpp
+++ Lesson_33_2_C++/Lesson_33_2_C++.cpp
@@ -1,1 +1,10 @@
-﻿
+﻿#include "Provider.h"
+
+int main()
+{
+	Provider kyivstar("Kyivstar");
+	kyivstar.addTariff(new SecondsTariff("day", 0.01));
+	kyivstar.addTariff(new MinutesTariff("night", 0.15));
+	kyivstar.showList();
+
+}
